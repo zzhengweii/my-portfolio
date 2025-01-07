60d91a9265a30994825cea9d8dd93480426d5acf
zzhengweii <ow.zhengwei@gmail.com>
2025-01-07 16:40:23 +0800

    particles

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import ParticlesComponent from "./components/particles";
+import Navbar from "./components/Nabvar";
+import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <nav>
+        <Navbar />
+      </nav>
+      <ParticlesComponent id="particles" />
+      <h1>Particles are so fun!</h1>
+    </div>
+  );
 }
 
 export default App;
