ce0354c2e76615882bb6ff25de83176d5b6fe794
zzhengweii <ow.zhengwei@gmail.com>
2025-01-07 16:58:35 +0800

    Revert "particles"
    This reverts commit 60d91a9265a30994825cea9d8dd93480426d5acf.

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import ParticlesComponent from "./components/particles";
-import Navbar from "./components/Nabvar";
-import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <Navbar />
-      </nav>
-      <ParticlesComponent id="particles" />
-      <h1>Particles are so fun!</h1>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
